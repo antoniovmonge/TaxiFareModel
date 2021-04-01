5f8ea72b159e6547098a54c5347d6820a84435c0
Antonio Vilchez Monge <antoniovmonge@gmail.com>
2021-04-01 16:40:38 +0200

    MLFlow quickstart

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -6,8 +6,17 @@ from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from mlflow.tracking import MlflowClient
+from memoized_property import memoized_property
+import mlflow
+import joblib
+
+MLFLOW_URI = "https://mlflow.lewagon.co/"
+myname = "antoniovmonge"
+EXPERIMENT_NAME = f"TaxifareModel_{myname}"
 
 class Trainer():
+
     def __init__(self, X, y):
         """
             X: pandas DataFrame
@@ -16,6 +25,12 @@ class Trainer():
         self.pipeline = None
         self.X = X
         self.y = y
+        # for MLFlow
+        self.experiment_name = EXPERIMENT_NAME
+        
+    def set_experiment_name(self, experiment_name):
+        '''defines the experiment name for MLFlow'''
+        self.experiment_name = experiment_name
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
@@ -46,12 +61,42 @@ class Trainer():
         """set and train the pipeline"""
         self.set_pipeline()
         self.pipeline.fit(self.X, self.y)
+        self.mlflow_log_param("model", "Linear")
 
     def evaluate(self, X_test, y_test):
         """evaluates the pipeline on df_test and return the RMSE"""
         y_pred = self.pipeline.predict(X_test)
         rmse = compute_rmse(y_pred, y_test)
+        self.mlflow_log_metric("rmse", rmse)
         return round(rmse, 2)
+    
+    def save_model(self):
+        """Save the model into a .joblib format"""
+        joblib.dump(self.pipeline, 'model.joblib')
+        print(colored("model.joblib saved locally", "green"))
+    
+    # MLFlow methods
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(MLFLOW_URI)
+        return MlflowClient()
+    
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
 
 
 if __name__ == "__main__":
@@ -67,3 +112,4 @@ if __name__ == "__main__":
     trainer.run()
     rmse = trainer.evaluate(X_test, y_test)
     print(f"rmse: {rmse}")
+    trainer.save_model()
